5c33fa7f8ae52c7fd69e1f4db345cc300a1de4a3
AndreGuerra <andreguerra@lojadomecanico.com.br>
2024-03-13 21:25:43 -0300

    add: implementation of homepage

--- front-end/ofco/lib/main.dart
+++ front-end/ofco/lib/main.dart
@@ -11,115 +11,194 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'OFCO',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  final GlobalKey key1 = GlobalKey();
+  final GlobalKey key2 = GlobalKey();
+  final GlobalKey key3 = GlobalKey();
+  final GlobalKey key4 = GlobalKey();
+  final GlobalKey key5 = GlobalKey();
 
-  void _incrementCounter() {
+  late final List listSections;
+
+  late GlobalKey selectedSectionKey;
+
+  void clickOnSection(int key) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      selectedSectionKey = listSections[key];
     });
+    scroll();
+  }
+
+  @override
+  void initState() {
+    listSections = [key1, key2, key3, key4, key5];
+    selectedSectionKey = key1;
+    super.initState();
+  }
+
+  void scroll() {
+    Scrollable.ensureVisible(
+      selectedSectionKey.currentContext!,
+      duration: const Duration(seconds: 1),
+      curve: Curves.ease,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            SectionMenu(
+              key: key1,
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 60),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      MenuItem(
+                        text: 'Início',
+                        index: 0,
+                        scroll: (i) => clickOnSection(i),
+                      ),
+                      MenuItem(
+                        text: 'Características',
+                        index: 1,
+                        scroll: (i) => clickOnSection(i),
+                      ),
+                      MenuItem(
+                        text: 'Campeonatos',
+                        index: 2,
+                        scroll: (i) => clickOnSection(i),
+                      ),
+                      MenuItem(
+                        text: 'Planos',
+                        index: 3,
+                        scroll: (i) => clickOnSection(i),
+                      ),
+                      MenuItem(
+                        text: 'Fale Conosco',
+                        index: 4,
+                        scroll: (i) => clickOnSection(i),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SectionMenu(
+              key: key2,
+              color: Colors.purple,
+              child: const SizedBox(),
+            ),
+            SectionMenu(
+              key: key3,
+              color: Colors.yellow,
+              child: const SizedBox(),
+            ),
+            SectionMenu(
+              key: key4,
+              color: Colors.red,
+              child: const SizedBox(),
+            ),
+            SectionMenu(
+              key: key5,
+              color: Colors.grey,
+              child: const SizedBox(),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class MenuItem extends StatefulWidget {
+  const MenuItem({
+    super.key,
+    required this.text,
+    required this.index,
+    required this.scroll,
+  });
+
+  final String text;
+  final int index;
+  final Function scroll;
+
+  @override
+  State<MenuItem> createState() => _MenuItemState();
+}
+
+Color color = Colors.white;
+
+class _MenuItemState extends State<MenuItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 40.0),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        onHover: (event) => setState(() {
+          color = const Color.fromARGB(255, 90, 255, 148);
+        }),
+        onExit: (event) => setState(() {
+          color = Colors.white;
+        }),
+        child: GestureDetector(
+          onTap: () {
+            widget.scroll(widget.index);
+          },
+          child: Text(
+            widget.text,
+            style: TextStyle(
+              color: color,
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SectionMenu extends StatelessWidget {
+  const SectionMenu({super.key, required this.child, required this.color});
+
+  final Widget child;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context).size;
+
+    return Container(
+      color: color,
+      height: mq.height,
+      width: double.infinity,
+      child: child,
     );
   }
 }
